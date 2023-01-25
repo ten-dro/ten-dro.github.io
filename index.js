7ce9c90f4dc3d205997e3921253520abdcf78e5d
Tendro012 <ermalkonjuhi12@gmail.com>
2023-01-24 19:50:51 -0800

    W rizz

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("men").addEventListener("click", function () {
+  window.location.href =
+    "https://discord.com/api/oauth2/authorize?client_id=970095499392729138&permissions=137439275072&scope=bot";
+});
