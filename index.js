213e959a3e7ebd57e7135d2616cd4ed7dc93ad98
Tendro012 <ermalkonjuhi12@gmail.com>
2023-01-24 20:04:40 -0800

    idk

--- index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("obama").addEventListener("click", function () {
-  window.location.href =
-    "https://discord.com/api/oauth2/authorize?client_id=970095499392729138&permissions=137439275072&scope=bot";
-});
-document.getElementById("hortum").addEventListener("click", function () {
-  window.location.href =
-    "https://discord.com/api/oauth2/authorize?client_id=978528781344063498&permissions=1634197695574&scope=bot";
-});
